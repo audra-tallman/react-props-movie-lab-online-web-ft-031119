a4a140206a64c86493df4a4b906eec3719077b69
Audra Tallman <audi.tallman@gmail.com>
2019-07-24 02:10:03 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -41,4 +41,9 @@ export default class MovieCard extends Component {
   }
 }
 
-
+MovieCard.defaultProps = {
+	title: 'Unknown',
+	IMDBRating: null,
+	genres: ['No Genre(s) Found'],
+	poster: 'default'
+};
