0b0cea4cb0c2573f3958b1d29445afe38038de79
Audra Tallman <audi.tallman@gmail.com>
2019-07-24 02:14:30 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,14 +5,14 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-   
-  }
+   return movieData.map((data, index) => <MovieCard key={index} {...data} />);
+  };
 
   render() {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
-      </div>
+      </div>;
     )
   }
 }
